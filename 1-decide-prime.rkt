b7f376b5ec6c13bf75b694982788513b96348f9b
KyleFeng <kylezete@gmail.com>
2018-02-03 19:42:21 +0000

    update solution for checking prime number

--- 1-decide-prime.rkt
+++ 1-decide-prime.rkt
@@ -5,16 +5,15 @@
 ; otherwise. Call the function decide-prime
 
 ; adapt from: https://stackoverflow.com/questions/3345626/finding-a-prime-number-in-scheme-using-natural-recursion
-; cond: (condition) https://docs.racket-lang.org/reference/if.html
 
 (define (decide-prime m)
     ; insert a loop function(checking if/not can be devide by [2 ~ m-1])
     (define (step a b)
-        (cond
-            ((= b 1) #t)
-            (else (if(eq? (modulo a b) 0)
+        (if (= b 1)
+            #t
+            (if (= (modulo a b) 0)
                 #f
-                (step a (- b 1))))))
+                (step a (- b 1)))))
     ; first check m is 1( 1 is not a prime number)
     (if (= m 1)
         #f
