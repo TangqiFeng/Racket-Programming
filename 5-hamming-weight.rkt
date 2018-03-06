b83142a8251efcbefaeecdaf19e0a5070d80a3f0
KyleFeng <kylezete@gmail.com>
2018-03-06 22:32:49 +0000

    add comments

--- 5-hamming-weight.rkt
+++ 5-hamming-weight.rkt
@@ -5,6 +5,7 @@
 ; > (hamming-weight (list 1 0 1 0 1 1 1 0))
 ; 5
 
+; remove zeros
 (define (rmZero l)
     (if (null? l)
         null
@@ -12,6 +13,7 @@
             (rmZero (cdr l))
             (cons (car l) (rmZero (cdr l))))))
 
+; use length function to get the number of 1s
 (define (hamming-weight l)
     (length (rmZero l)))
 
