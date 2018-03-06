ffd558a8b50081ddcbe0e4c0c527f6deedae5ddd
KyleFeng <kylezete@gmail.com>
2018-03-06 22:30:20 +0000

    add solution for 5-hamming-weight

---
+++ 5-hamming-weight.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+; Write a function hamming-weight in Racket that takes a list l as input and returns
+; the number of non-zero elements in it. For example:
+; > (hamming-weight (list 1 0 1 0 1 1 1 0))
+; 5
+
+(define (rmZero l)
+    (if (null? l)
+        null
+        (if (= (car l) 0)
+            (rmZero (cdr l))
+            (cons (car l) (rmZero (cdr l))))))
+
+(define (hamming-weight l)
+    (length (rmZero l)))
+
+(hamming-weight (list 1 0 1 0 1 1 1 0))
